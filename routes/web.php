71764984e1f7422cd4d6fd26b68d766e941c9257
rahim8050 <rahimranxx8050@gmail.com>
2025-01-21 19:37:59 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Inertia\Inertia;
+use App\Http\Controllers\Display;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\DisplayController;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('/index/search', [DisplayController::class,'index'])->name('index');
+Route::resource('index', DisplayController::class);
+
+require __DIR__.'/auth.php';
